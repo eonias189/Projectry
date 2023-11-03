d94878483da4e819f8bdbdfff86c0a6b7168c4e6
eonias189 <eonias189@yandex.ru>
2023-11-03 21:43:08 +0400

    formated files & added electron-forge to make app

--- window/renderer.js
+++ window/renderer.js
@@ -1,8 +1,11 @@
-const themeSwitcher = document.getElementById('theme-switcher');
+const themeSwitcher = document.getElementById("theme-switcher");
 const body = document.body;
 
-themeSwitcher.addEventListener('click', async () => {
-    body.classList.toggle('dark');
-    let appl = await app();
-    alert(appl.name);
+themeSwitcher.addEventListener("click", async () => {
+    body.classList.toggle("dark");
+    setTimeout(() => {
+        app().then((appl) => {
+            alert(appl.name);
+        });
+    }, 2000);
 });
