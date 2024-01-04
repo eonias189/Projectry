733f09e0707885f9288f16268ab5eea1627130b7
eonias189 <eonias189@yandex.ru>
2024-01-04 18:47:50 +0400

    router bug fixed

--- src/components/router.tsx
+++ src/components/router.tsx
@@ -1,9 +1,21 @@
-import React, { FC } from "react";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { FC, useEffect } from "react";
+import { HashRouter, Routes, Route, useNavigate } from "react-router-dom";
 import MainPage from "../pages/mainPage";
 import DevPage from "../pages/devPage";
 
 interface RouterProps {}
+
+const RedirectToMainPage: FC = () => {
+    const navigate = useNavigate();
+    useEffect(() => {
+        navigate("/");
+    });
+    return (
+        <div>
+            <h1>Redirecting to main page</h1>
+        </div>
+    );
+};
 
 const Router: FC<RouterProps> = ({}) => {
     return (
@@ -11,6 +23,7 @@ const Router: FC<RouterProps> = ({}) => {
             <Routes>
                 <Route path="/" element={<MainPage />} />
                 <Route path="/dev/:project_name" element={<DevPage />} />
+                <Route path="/dev/" element={<RedirectToMainPage />} />
             </Routes>
         </HashRouter>
     );
