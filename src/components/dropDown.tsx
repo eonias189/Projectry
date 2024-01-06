e1e3d5f50dfe0f4fee564cf54f717a3292c169cf
eonias189 <eonias189@yandex.ru>
2024-01-07 01:51:40 +0400

    icons placed to separete ui module component

--- src/components/dropDown.tsx
+++ src/components/dropDown.tsx
@@ -1,6 +1,6 @@
 import { FC, ReactElement, ReactNode, useState } from "react";
 import styles from "./dropDown.module.css";
-import IconBtn from "./UI/iconBtn";
+import { expandedIcon, collapsedIcon } from "./UI/icons";
 
 interface DropDownProps {
     children?: ReactNode;
@@ -10,17 +10,6 @@ interface DropDownProps {
 
 const DropDown: FC<DropDownProps> = ({ children, primaryElement, className }) => {
     const [expanded, setExpanded] = useState(false);
-
-    const collapsedIcon = (
-        <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512">
-            <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
-        </svg>
-    );
-    const expandedIcon = (
-        <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512">
-            <path d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z" />
-        </svg>
-    );
 
     return (
         <div className={className}>
